963cec0225647a8591a31a58f6aa355ab0300164
unknown <cintiasilveirarj@gmail.com>
2023-02-13 16:28:49 -0300

    componente Header concluido

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react';
 import Api from './Api';
 import MovieRow from './Components/MovieRow';
 import FeaturedMovie from './Components/FeaturedMovie';
+import Header from  './Components/Header'
 import './App.css';
 
 function App() {
   const [movieList, setMovieList] = useState([]);
   const[featuredData,setFeatureData] = useState(null);
+  const[blackHeader, setBlackHeader] = useState(false)
 
     useEffect(() => {
       const loadAall = async () => {
@@ -23,11 +25,29 @@ function App() {
         setFeatureData(chosenInfo)
       }
       loadAall();
+
+    }, [])
+
+    // Monitorar o Scroll para alteração do Header
+    useEffect(() => {
+      const scrollListener = () => {
+
+        if(window.scrollY > 10 ){
+          setBlackHeader(true)
+        } else {
+          setBlackHeader(false)
+        }
+      }
+      window.addEventListener('scroll', scrollListener)
       
+      return () => {
+        window.removeEventListener('scroll', scrollListener)
+      }
     }, [])
 
   return (
     <div className='page'>
+      <Header black={blackHeader} />
       {featuredData &&
         <FeaturedMovie item={featuredData} />
       }
